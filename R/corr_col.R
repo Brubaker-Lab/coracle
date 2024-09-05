6b8be6f49e0888cb07f827e7a446cad8cc915129
raykrajci <Raymond.Krajci@gmail.com>
2024-09-05 16:12:00 -0400

    Initial Commit

---
+++ R/corr_col.R
@@ -0,0 +1,14 @@
+#' Correlate all columns of two data frames
+#'
+#' @param x_data A data frame with at least one shared index column and at least one column of numeric data.
+#' @param y_data A data frame with at least one shared index column and at least one column of numeric data.
+#' @param ...
+#'
+#' @return A data frame of correlation results.
+#' @export
+#'
+#' @examples
+#' corr_col(df1, df2)
+corr_col <- function(x_data, y_data, ..., x_name, y_name){
+
+}
