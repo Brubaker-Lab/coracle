0c9d58bd8e26a184d3a06580b8fcfe1bbc219b22
raykrajci <Raymond.Krajci@gmail.com>
2025-04-02 15:27:34 -0400

    MVP of coracle_data

--- R/coracle_data.R
+++ R/coracle_data.R
@@ -12,7 +12,6 @@
 coracle_data <- R6::R6Class(
   "coracle_data",
   public = list(
-
     ## Fields ------------
 
     #' @field version Package version used to generate node.
@@ -26,9 +25,6 @@ coracle_data <- R6::R6Class(
 
     #' @field children Child `coracle_data` node(s).
     children = NULL,
-
-    #' @field parent Parent `coracle_data` node.
-    parent = NULL,
 
     #' @field grps_cols Grouping column(s) in data for this node and it's children.
     grps_cols = NULL,
@@ -45,33 +41,162 @@ coracle_data <- R6::R6Class(
     #' @field other_cols Other column(s) present in data.
     other_cols = NULL,
 
-## Initalization ------------
+    debug = NULL,
+
+    ## Initalization ------------
 
     #' @description
     #' Create a new `coracle_data` node
     #'
     #'
     #' @param data
-    #' @param grps_cols
-    #' @param join_col
-    #' @param vals_cols
-    #' @param vals_labl
     #' @param call
+    #' @param grps
+    #' @param join
+    #' @param vals
     #'
     #' @returns
     #'
     #' @examples
-    initialize = function(data,
-                          grps_cols,
-                          join_col,
-                          vals_cols,
-                          vals_labl,
+    initialize = function(data = NULL,
+                          grps = NULL,
+                          join = NULL,
+                          vals = NULL,
+                          ...,
+                          labl_cols = NULL,
+                          labl_vals = NULL,
                           call = caller_env()) {
+      ### Initial data check ------------
 
+      if (!is.data.frame(data))
+        cli_abort(c("x" = "{.arg data} requires a {.cls data.frame}."), call = call)
+
+      ### Tidyselect Group Column(s) ------------
+
+      grps_expr <- rlang::enquo(grps)
+      grps_pos <- tidyselect::eval_select(grps_expr, data)
+
+      if (length(grps_pos) < 1)
+        cli_abort(c("x" = "{.arg grps} requires a selection."), call = call)
+
+      self$grps_cols <- names(grps_pos)
+
+      ### Tidyselect Join Column ------------
+
+      join_expr <- rlang::enquo(join)
+      join_pos <- tidyselect::eval_select(join_expr, data)
+
+      if (length(join_pos) != 1)
+        cli_abort(c("x" = "{.arg join} must select exactly one column."), call = call)
+
+      self$join_col <- names(join_pos)
+      self$join_vals <- sort(unique(data[[self$join_col]]))
+
+      ### Tidyselect Variable Column(s) ------------
+
+      vals_expr <- rlang::enquo(vals)
+      vals_pos <- tidyselect::eval_select(vals_expr, data)
+
+      if (length(vals_pos) < 1)
+        cli_abort(c("x" = "{.arg vals} requires a selection."), call = call)
+
+      ### Ensure no overlapping selections ------------
+
+      if (length(intersect(grps_pos, join_pos)) != 0)
+        cli_abort(c("x" = "{.arg grps} and {.arg join} may not share any column(s)."),
+                  call = call)
+
+      if (length(intersect(grps_pos, vals_pos)) != 0)
+        cli_abort(c("x" = "{.arg grps} and {.arg vals} may not share any column(s)."),
+                  call = call)
+
+      if (length(intersect(join_pos, vals_pos)) != 0)
+        cli_abort(c("x" = "{.arg join} and {.arg vals} may not share any column(s)."),
+                  call = call)
+
+      ### Other Column(s)
+
+      self$other_cols <- subset(data, select = -c(join_pos, vals_pos, grps_pos)) |>
+        names()
+
+      ### Reassemble data ------------
+
+      data <- data |>
+        dplyr::relocate(!!grps_expr, !!join_expr, !!vals_expr)
+
+      ### Transform data to long format, if necessary ------------
+
+      if (length(vals_pos) > 1) {
+
+        if (!is.null(labl_cols) && !is_scalar_character(labl_cols))
+            cli_abort(c("x" = "{.arg labl_cols} requires a {.cls character} scalar."),
+                      call = call)
+
+        if (!is.null(labl_vals)) {
+          if (!is_scalar_character(labl_vals)) {
+            cli_abort(c("x" = "{.arg labl_vals} requires a {.cls character} scalar."),
+                      call = call)
+          }
+        }
+
+        name_col <- labl_cols %||% "names"
+        values_col <- labl_vals %||% "values"
+
+        data <- data |> tidyr::pivot_longer(
+          cols = all_of(names(vals_pos)),
+          names_to = name_col,
+          values_to = values_col
+        )
+
+        self$grps_cols <- c(self$grps_cols, name_col)
+        self$vals_col <- values_col
+
+      } else {
+        # If not necessary
+        self$vals_col <- names(vals_pos)
+      }
+
+      ### Reorder columns -> grps, join, vals, other ------------
+
+      data <- data |>
+        dplyr::relocate(all_of(self$grps_cols), self$join_col, self$vals_col, all_of(self$other_cols))
+
+      ### Chunk by grps, creating tree ------------
+
+      for(col in self$grps_cols)
+      {
+
+        col_values <- data[[col]]
+        col_values <- col_values[!is.na(col_values)]
+
+        if(length(unique(col_values)) > 1){
+
+          self$children <- sort(unique(col_values)) |>
+            purrr::set_names() |>
+            map(\(x)
+                 coracle_data$new(data = data[data[[col]] == x,],
+                                  grps = self$grps_cols,
+                                  join = self$join_col,
+                                  vals = self$vals_col,
+                                  labl_cols = labl_cols,
+                                  labl_vals = labl_vals
+                                  )
+                 )
+
+          break
+        }
+      }
+
+      if(is.null(self$children)){
+        self$chunk <- data
+      }
+
+      self$id <- rlang::hash(as.numeric(Sys.time())) |> stringr::str_sub(-7)
+      self$version <- as.character(packageVersion("coracle"))
     }
   ),
 
-## Active Bindings ------------
+  ## Active Bindings ------------
 
   active = list(
     #' @description
@@ -85,8 +210,10 @@ coracle_data <- R6::R6Class(
     #' @examples
     chunks = function(node = self) {
 
-    },
+      map(self$leaves,
+          \(x) x$chunk)
 
+    },
 
     #' @description
     #' Get data as a data frame.
@@ -99,6 +226,9 @@ coracle_data <- R6::R6Class(
     #' @examples
     data = function(node = self) {
 
+      self$chunks |>
+        purrr::list_rbind()
+
     },
 
     #' @description
@@ -110,7 +240,11 @@ coracle_data <- R6::R6Class(
     #'
     #' @examples
     leaves = function(node = self) {
-
+      if (!is.null(node$chunk)) {
+        return(node)
+      } else {
+        map(self$children, \(x) x$leaves) |> purrr::list_flatten()
+      }
     }
   )
 )
--- tests/testthat/test-coracle_data.R
+++ tests/testthat/test-coracle_data.R
@@ -24,17 +24,45 @@ df2 <- expand_grid(drug = LETTERS[22:26], gene = letters[1:26]) %>%
   ungroup()
 
 test_that("coracle_data.R", {
-  expect_true(exists("coracle_data"))
+  expect_true(exists("coracle_data")) # Prevent empty test warning
 
-  f <- coracle_data
+  f <- suppressMessages(coracle_data)
 
   test_that("Initializaton", {
-      expect_error(f$new(NULL))
-      expect_error(f$new(1))
-      expect_error(f$new(list(1, 2, 3)))
-      expect_error(f$new(mtcars, join = NULL, disp, mpg))
-      expect_error(f$new(mtcars, cyl, vars = NULL, mpg))
-      expect_error(f$new(mtcars, cyl, disp, NULL))
+    expect_true(exists("f")) # Prevent empty test warning
+
+    test_that("Erroneous Inputs", {
+      expect_true(exists("f")) # Prevent empty test warning
+
+      test_that("Arg `data` is a data.frame", {
+        expect_snapshot(f$new(NULL), error = T)
+        expect_snapshot(f$new(1), error = T)
+        expect_snapshot(f$new(list(1, 2, 3)), error = T)
+
+      })
+
+      test_that("Arg `join` selects exactly one column", {
+        expect_snapshot(f$new(mtcars, join = NULL, disp, mpg), error = T) # No join
+        expect_snapshot(f$new(mtcars, join = c(cyl, drat), disp, mpg), error = T) # Multiple joins
+        expect_snapshot(f$new(mtcars, join = abcd, disp, mpg), error = T) # join not in data
+
+      })
+
+
+      test_that("Arg `vals` selects one or more column(s)", {
+        expect_snapshot(f$new(mtcars, cyl, vals = NULL, mpg), error = T) # No vals
+        expect_snapshot(f$new(mtcars, cyl, vals = abcd, mpg), error = T) # vals not in data
+      })
+
+      test_that("Arg `grps` selects one or more column(s)", {
+        expect_snapshot(f$new(mtcars, cyl, disp, grps = NULL), error = T) # No grps
+        expect_snapshot(f$new(mtcars, cyl, disp, grps = abcd), error = T) # grps not in data
+      })
+
+
+
+    })
+
   })
 
 })
